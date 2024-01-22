c2bcab44cc414366062386ea0f5335f9ea35a1d6
NickPaterson <Npgb18@gmail.com>
2024-01-22 15:40:10 +0000

    updated to ES6

--- create-npmrc.js
+++ create-npmrc.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
-const process = require('process'); // Import the 'process' module
-const fontAwesomeToken = process.env.FONTAWESOME_NPM_TOKEN;
+import { writeFileSync } from 'fs';
+import { env } from 'process'; // Import the 'process' module
+const fontAwesomeToken = env.FONTAWESOME_NPM_TOKEN;
 
 const npmrcContent = `@fortawesome:registry=https://npm.fontawesome.com/\n//npm.fontawesome.com/:_authToken=${fontAwesomeToken}\n`;
 
-fs.writeFileSync('.npmrc', npmrcContent);
+writeFileSync('.npmrc', npmrcContent);
