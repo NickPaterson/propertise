fb6741a41e4ec0dda2eb37baf8de6f2525db073a
NickPaterson <Npgb18@gmail.com>
2024-01-22 16:08:19 +0000

    .

--- create-npmrc.js
+++ create-npmrc.js
@@ -1,11 +1,10 @@
 import fs from 'fs';
 import process from 'process';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const fontAwesomeToken = process.env.FONTAWESOME_NPM_TOKEN;
-console.log('fontAwesomeToken', fontAwesomeToken);
+if (!fontAwesomeToken) {
+  throw new Error('FONTAWESOME_NPM_TOKEN environment variable is not set');
+}
 const npmrcContent = `@fortawesome:registry=https://npm.fontawesome.com/\n//npm.fontawesome.com/:_authToken=${fontAwesomeToken}\n`;
 
 fs.writeFileSync('.npmrc', npmrcContent);
