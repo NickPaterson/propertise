53d8fb0da11fb97798e86f8a3ad530dda5b3d1c6
NickPaterson <Npgb18@gmail.com>
2024-01-22 15:54:16 +0000

    npmrc js updated to read from env file correctly

--- create-npmrc.js
+++ create-npmrc.js
@@ -1,7 +1,11 @@
-import { writeFileSync } from 'fs';
-import { env } from 'process'; // Import the 'process' module
-const fontAwesomeToken = env.FONTAWESOME_NPM_TOKEN;
+import fs from 'fs';
+import process from 'process';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const fontAwesomeToken = process.env.FONTAWESOME_NPM_TOKEN;
 
 const npmrcContent = `@fortawesome:registry=https://npm.fontawesome.com/\n//npm.fontawesome.com/:_authToken=${fontAwesomeToken}\n`;
 
-writeFileSync('.npmrc', npmrcContent);
+fs.writeFileSync('.npmrc', npmrcContent);
