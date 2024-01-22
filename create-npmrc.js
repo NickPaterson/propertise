658bb84ca3ca5541092ad248deb31c120661a83e
NickPaterson <Npgb18@gmail.com>
2024-01-22 16:03:40 +0000

    console test

--- create-npmrc.js
+++ create-npmrc.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const fontAwesomeToken = process.env.FONTAWESOME_NPM_TOKEN;
-
+console.log('fontAwesomeToken', fontAwesomeToken);
 const npmrcContent = `@fortawesome:registry=https://npm.fontawesome.com/\n//npm.fontawesome.com/:_authToken=${fontAwesomeToken}\n`;
 
 fs.writeFileSync('.npmrc', npmrcContent);
