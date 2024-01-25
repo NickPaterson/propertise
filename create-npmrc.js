1006e734e0c7a30c10c0c5ba90ccc40a7a662022
NickPaterson <Npgb18@gmail.com>
2024-01-25 21:34:16 +0000

    dotenv added to js files with process.env

--- create-npmrc.js
+++ create-npmrc.js
@@ -1,8 +1,10 @@
 import fs from 'fs';
 import process from 'process';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const fontAwesomeToken = process.env.FONTAWESOME_NPM_TOKEN;
-console.log("Creating .npmrc with FONTAWESOME_NPM_TOKEN:", process.env.FONTAWESOME_NPM_TOKEN);
+console.log("Creating .npmrc with FONTAWESOME_NPM_TOKEN:", fontAwesomeToken);
 
 const npmrcContent = `@fortawesome:registry=https://npm.fontawesome.com/\n//npm.fontawesome.com/:_authToken=${fontAwesomeToken}\n`;
 
